a057f8242a6f30128a477b17ffe22645ee0d4eca
Warren Boult <wmboult94@gmail.com>
2025-02-13 11:36:10 +0000

    add audio recording / upload / viewing

--- streamlit_app_audio.py
+++ streamlit_app_audio.py
@@ -3,4 +3,13 @@ import pandas as pd
 
 st.title("🐘 AUDIO LLM CHATBOT INCOMING!!! 🐘")
 
-st.audio_input("Upload a clip of the patient's issue")
+recorded_audio = st.audio_input("Record or upload the patient's issue")
+uploaded_audio = st.file_uploader("Or upload an audio file", type=["mp3"])
+
+audio_data = recorded_audio if recorded_audio else uploaded_audio
+
+if audio_data:
+    st.write("Audio input received:")
+    st.audio(audio_data)
+
+
